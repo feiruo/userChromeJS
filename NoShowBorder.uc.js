105e3bd2c54f1c96119ffbbb90960da310abbc25
feiruo <zk191201187@126.com>
2014-02-12 22:35:35 +0800

    可能导致某些UI调整失效

---
+++ NoShowBorder.uc.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name            noshowborder.uc.js
+// @namespace       firefox
+// @description     无边框
+// @include         chrome://browser/content/browser.xul
+// @version         1.1
+// ==/UserScript==
+(function() {
+	window.chromemargin = {
+		init: function() {
+			window.addEventListener("aftercustomization", this, false);
+			window.addEventListener("customizationchange", this, false);
+		},
+		handleEvent: function(evnet) {
+			document.documentElement.setAttribute("chromemargin", "0,7,7,7");
+		}
+	};
+	window.chromemargin.init();
+
+	setTimeout(function() {
+		document.documentElement.setAttribute("chromemargin", "0,7,7,7");
+	}, 500);
+})();
