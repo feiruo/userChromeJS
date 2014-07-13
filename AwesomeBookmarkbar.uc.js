e89510b0d9ffc6ca895b74d44d9516c554cebedf
feiruo <zk191201187@126.com>
2014-07-13 21:36:15 +0800

    +鼠标滑过地址栏自动显示，移出隐藏

--- AwesomeBookmarkbar.uc.js
+++ AwesomeBookmarkbar.uc.js
@@ -4,44 +4,53 @@
 // @homepage       https://github.com/feiruo/userchromejs/
 // @author         feiruo
 // @charset        utf-8
-// @version        0.1
+// @version        0.2
+// @compatibility  Firefox 24.0
+// @note           0.2 增加鼠标移到地址栏自动显示书签工具栏，移出隐藏
 // @note           点击地址栏显示书签工具栏。
 // @note           地址栏任意按键，地址栏失去焦点后自动隐藏书签工具栏。
 // @note           左键点击书签后自动隐藏书签工具栏。
 // ==/UserScript== 
 (function() {
-	var PersonalToolbar = document.getElementById("PersonalToolbar");
+	var PTBar = document.getElementById("PersonalToolbar");
 	var placesCommands = document.getElementById("placesCommands");
 
-	setToolbarVisibility(PersonalToolbar, PersonalToolbar.collapsed);
+	setToolbarVisibility(PTBar, PTBar.collapsed);
 
 	function hideToolbar(e) {
 		if (e.button == 2 || (e.button == 0 && !(e.metaKey || e.shiftKey || e.ctrlKey))) return;
-		PersonalToolbar.collapsed = true;
+		PTBar.collapsed = true;
 	}
 
-	PersonalToolbar.addEventListener("command", hideToolbar, false);
-	PersonalToolbar.addEventListener("click", hideToolbar, false);
+	PTBar.addEventListener("command", hideToolbar, false);
+	PTBar.addEventListener("click", hideToolbar, false);
 	placesCommands.addEventListener("command", hideToolbar, false);
-
-	setToolbarVisibility(PersonalToolbar, PersonalToolbar.collapsed);
 
 	gURLBar.addEventListener('click', function(e) {
 		if (e.button == 0) {
-			var pbar = document.getElementById('PersonalToolbar');
-			if (pbar.collapsed) {
-				pbar.collapsed = !pbar.collapsed
-			}
+			PTBar.collapsed = false;
 		}
 	}, false);
 
-	gURLBar.addEventListener('blur', function(e) {
-		document.getElementById('PersonalToolbar').setAttribute('collapsed', 'true');
+	PTBar.addEventListener('mouseover', function(e) {
+		PTBar.setAttribute('collapsed', 'false');
+	}, false);
+
+	PTBar.addEventListener('mouseout', function(e) {
+		PTBar.setAttribute('collapsed', 'true');
+	}, false);
+
+	gURLBar.addEventListener('mouseover', function(e) {
+		PTBar.setAttribute('collapsed', 'false');
+	}, false);
+
+	gURLBar.addEventListener('mouseout', function(e) {
+		PTBar.setAttribute('collapsed', 'true');
 	}, false);
 
 	gURLBar.addEventListener('keydown', function(e) {
 		if (window.event ? e.keyCode : e.which)
-			document.getElementById('PersonalToolbar').setAttribute('collapsed', 'true');
+			PTBar.collapsed = true;
 	}, false);
 
 })();
