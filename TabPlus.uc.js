9d2be69652b9ba9744bb71df00f7754bd71bb23d
FeiRuo <feiruosama@gmail.com>
2015-02-13 22:42:27 +0800

    更新，增加，修复

--- TabPlus.uc.js
+++ TabPlus.uc.js
@@ -7,11 +7,13 @@
 // @include				chrome://browser/content/bookmarks/bookmarksPanel.xul
 // @include				chrome://browser/content/history/history-panel.xul
 // @include				chrome://browser/content/places/places.xul
+// @include        		chrome://mozapps/content/downloads/unknownContentType.xul
+// @include        		chrome://mozapps/content/downloads/downloads.xul
 // @id              	[5C5BB610]
 // @startup       		window.TabPlus_mod.init();
 // @shutdown      		window.TabPlus_mod.onDestroy(true);
 // @homepageURL   		https://github.com/feiruo/userchromejs/
-// @version      		0.1
+// @version      		0.2
 // @note 				新しいタブで開く（空白タブを利用）
 // @note 				新标签打开（智能利用空白标签）
 // @note				必要なものだけ使って不要であれば削除するなりコメントアウトして下さい
@@ -22,15 +24,20 @@
 		window.TabPlus_mod.onDestroy();
 		delete window.TabPlus_mod;
 	}
+
 	var TabPlus_mod = {
-		gURLBar: null,
-		openLinkIn: null,
-		whereToOpenLink: null,
+		gURLBar: gURLBar.handleCommand.toString(),
+		openLinkIn: openLinkIn.toString(),
+		whereToOpenLink: whereToOpenLink.toString(),
+		cKBOnMCa: BookmarksEventHandler.onClick.toString(),
+		cKBOnMCb: checkForMiddleClick.toString(),
+		cdbt: gBrowser.mTabProgressListener.toString(),
+		tab_hover: null,
 	};
+
 	TabPlus_mod.init = function() {
 		this.urlbarNewTab(true); // 地址栏新标签打开
-		this.historyNewTab(true); //新标签打开书签，历史和搜索栏
-		this.whereToOpen(true); // 中クリックでフォーカスを反転
+		this.historyNewTab(true); //新标签打开书签，历史和搜索栏		
 		this.ldblclickC(true); //左键双击标签关闭
 		this.mouseScroll(true); //滚轮切换标签
 		this.rclickC(true); //标签上点击鼠标右键关闭标签
@@ -40,7 +47,13 @@
 		//this.ldblclickRU(true); //未加载标签上双击刷新
 		//this.nearTab(true); //紧邻当前标签新建标签页
 		//this.cOLTab(true); //关闭标签聚焦左侧标签
+		//------------------------------------------------
+		this.whereToOpen(true); // 中クリックでフォーカスを反転
+		this.KeepBookmarksOnMiddleClick(true); // 鼠标中键点击时bookmark菜单不关闭
+		this.closeDownloadBankTab(true); // 自动关闭下载产生的空白标签
+		this.tabfocus(true); // 鼠标悬停激活标签
 	};
+
 	TabPlus_mod.onDestroy = function(isOk) {
 		this.urlbarNewTab(false);
 		this.historyNewTab(false);
@@ -48,17 +61,23 @@
 		this.ldblclickC(false);
 		this.mouseScroll(false);
 		this.rclickC(false);
-		//this.mMousedownR(false);
-		//this.mMousedownP(false);
-		//this.ldblclickR(false);
-		//this.ldblclickRU(false);
+		this.mMousedownR(false);
+		this.mMousedownP(false);
+		this.ldblclickR(false);
+		this.ldblclickRU(false);
 		this.nearTab(false);
+		this.cOLTab(false);
+		this.KeepBookmarksOnMiddleClick(false);
+		this.closeDownloadBankTab(false);
+		this.tabfocus(false);
 	};
+
+	//--------------------------------------------------------------------------
+
 	TabPlus_mod.urlbarNewTab = function(isOk) {
 		if (isOk) {
 			try {
-				this.gURLBar = gURLBar.handleCommand.toString();
-				location == "chrome://browser/content/browser.xul" && eval("gURLBar.handleCommand=" + gURLBar.handleCommand.toString().replace(/^\s*(load.+);/gm, "if(/^javascript:/.test(url)||isTabEmpty(gBrowser.selectedTab)){loadCurrent();}else{this.handleRevert();gBrowser.loadOneTab(url, {postData: postData, inBackground: false, allowThirdPartyFixup: true});}"));
+				location == "chrome://browser/content/browser.xul" && eval("gURLBar.handleCommand=" + this.gURLBar.replace(/^\s*(load.+);/gm, "if(/^javascript:/.test(url)||isTabEmpty(gBrowser.selectedTab)){loadCurrent();}else{this.handleRevert();gBrowser.loadOneTab(url, {postData: postData, inBackground: false, allowThirdPartyFixup: true});}"));
 			} catch (e) {}
 		} else {
 			try {
@@ -66,11 +85,11 @@
 			} catch (e) {}
 		}
 	};
+
 	TabPlus_mod.historyNewTab = function(isOk) {
 		if (isOk) {
 			try {
-				this.openLinkIn = openLinkIn.toString();
-				eval('openLinkIn=' + openLinkIn.toString().replace('w.gBrowser.selectedTab.pinned', '(!w.isTabEmpty(w.gBrowser.selectedTab) || $&)').replace(/&&\s+w\.gBrowser\.currentURI\.host != uriObj\.host/, ''));
+				eval('openLinkIn=' + this.openLinkIn.replace('w.gBrowser.selectedTab.pinned', '(!w.isTabEmpty(w.gBrowser.selectedTab) || $&)').replace(/&&\s+w\.gBrowser\.currentURI\.host != uriObj\.host/, ''));
 			} catch (e) {}
 		} else {
 			try {
@@ -78,11 +97,11 @@
 			} catch (e) {}
 		}
 	};
+
 	TabPlus_mod.whereToOpen = function(isOk) {
 		if (isOk) {
 			try {
-				this.whereToOpenLink = whereToOpenLink.toString();
-				eval('whereToOpenLink=' + whereToOpenLink.toString().replace(' || middle && middleUsesTabs', '').replace('if (alt', 'if (middle && middleUsesTabs) return shift ? "tab" : "tabshifted"; $&'));
+				eval('whereToOpenLink=' + this.whereToOpenLink.replace(' || middle && middleUsesTabs', '').replace('if (alt', 'if (middle && middleUsesTabs) return shift ? "tab" : "tabshifted"; $&'));
 			} catch (e) {}
 		} else {
 			try {
@@ -90,147 +109,245 @@
 			} catch (e) {}
 		}
 	};
+
 	TabPlus_mod.ldblclickC = function(isOk) {
-		function acction(event) {
-			if (event.target.localName == 'tab' && event.button == 0)
-				gBrowser.removeTab(event.target);
-		}
-
 		if (isOk) {
-			gBrowser.mTabContainer.addEventListener('dblclick', acction, true);
+			try {
+				gBrowser.mTabContainer.addEventListener('dblclick', TabPlus_mod.ldblclickC.acction, true);
+			} catch (e) {}
 		} else {
-			gBrowser.mTabContainer.removeEventListener('dblclick', acction, true);
+			try {
+				gBrowser.mTabContainer.removeEventListener('dblclick', TabPlus_mod.ldblclickC.acction, true);
+			} catch (e) {}
 		}
 	};
+	TabPlus_mod.ldblclickC.acction = function(event) {
+		if (event.target.localName == 'tab' && event.button == 0)
+			gBrowser.removeTab(event.target);
+	};
+
 	TabPlus_mod.mouseScroll = function(isOk) {
-		function acction(event) {
-			this.advanceSelectedTab(event.detail > 0 ? +1 : -1, true);
-		}
-
 		if (isOk) {
-			gBrowser.mTabContainer.addEventListener('DOMMouseScroll', acction, true);
+			try {
+				gBrowser.mTabContainer.addEventListener('DOMMouseScroll', TabPlus_mod.mouseScroll.acction, true);
+			} catch (e) {}
 		} else {
-			gBrowser.mTabContainer.removeEventListener('DOMMouseScroll', acction, true);
+			try {
+				gBrowser.mTabContainer.removeEventListener('DOMMouseScroll', TabPlus_mod.mouseScroll.acction, true);
+			} catch (e) {}
 		}
 	};
+	TabPlus_mod.mouseScroll.acction = function(event) {
+		this.advanceSelectedTab(event.detail > 0 ? +1 : -1, true);
+	};
+
 	TabPlus_mod.rclickC = function(isOk) {
-		function acction(event) {
-			if (event.target.localName == "tab" && event.button == 2 && !event.ctrlKey) {
-				gBrowser.removeTab(event.target);
-				event.stopPropagation();
-				event.preventDefault();
-			}
-		}
-
 		if (isOk) {
-			gBrowser.mTabContainer.addEventListener('click', acction, true);
+			try {
+				gBrowser.mTabContainer.addEventListener('click', TabPlus_mod.rclickC.acction, true);
+			} catch (e) {}
 		} else {
-			gBrowser.mTabContainer.removeEventListener('click', acction, true);
+			try {
+				gBrowser.mTabContainer.removeEventListener('click', TabPlus_mod.rclickC.acction, true);
+			} catch (e) {}
 		}
 	};
-	// -------------------------------------------------------------------------
+	TabPlus_mod.rclickC.acction = function(event) {
+		if (event.target.localName == "tab" && event.button == 2 && !event.ctrlKey) {
+			gBrowser.removeTab(event.target);
+			event.stopPropagation();
+			event.preventDefault();
+		}
+	};
+
+	//--------------------------------------------------------------------------
+
 	TabPlus_mod.mMousedownR = function(isOk) {
-		function acction(event) {
-			if (event.target.localName != 'tab' && event.button == 1) {
-				document.getElementById('History:UndoCloseTab').doCommand();
-			}
+		if (isOk) {
+			try {
+				gBrowser.mTabContainer.addEventListener('mousedown', TabPlus_mod.mMousedownR.acction, false);
+			} catch (e) {}
+		} else {
+			try {
+				gBrowser.mTabContainer.removeEventListener('mousedown', TabPlus_mod.mMousedownR.acction, false);
+			} catch (e) {}
 		}
+	};
+	TabPlus_mod.mMousedownR.acction = function(event) {
+		if (event.target.localName != 'tab' && event.button == 1) {
+			document.getElementById('History:UndoCloseTab').doCommand();
+		}
+	};
 
-		if (isOk) {
-			gBrowser.mTabContainer.addEventListener('mousedown', acction, false);
-		} else {
-			gBrowser.mTabContainer.removeEventListener('mousedown', acction, false);
-		}
-	};
 	TabPlus_mod.mMousedownP = function(isOk) {
-		function acction(event) {
-			if (event.target.localName == "tab" && event.button == 1 && !event.ctrlKey) {
-				var subTab = event.originalTarget;
-				while (subTab.localName != "tab") {
-					subTab = subTab.parentNode;
-				}
-				if (subTab.pinned) {
-					gBrowser.unpinTab(subTab);
-				} else {
-					gBrowser.pinTab(subTab);
-				}
-				event.stopPropagation();
-				event.preventDefault();
-			}
-		}
 		if (isOk) {
-			gBrowser.mTabContainer.addEventListener('click', acction, true);
+			try {
+				gBrowser.mTabContainer.addEventListener('click', TabPlus_mod.mMousedownP.acction, true);
+			} catch (e) {}
 		} else {
-			gBrowser.mTabContainer.removeEventListener('click', acction, true);
+			try {
+				gBrowser.mTabContainer.removeEventListener('click', TabPlus_mod.mMousedownP.acction, true);
+			} catch (e) {}
 		}
 	};
+	TabPlus_mod.mMousedownP.acction = function(event) {
+		if (event.target.localName == "tab" && event.button == 1 && !event.ctrlKey) {
+			var subTab = event.originalTarget;
+			while (subTab.localName != "tab") {
+				subTab = subTab.parentNode;
+			}
+			if (subTab.pinned) {
+				gBrowser.unpinTab(subTab);
+			} else {
+				gBrowser.pinTab(subTab);
+			}
+			event.stopPropagation();
+			event.preventDefault();
+		}
+	};
+
 	TabPlus_mod.ldblclickR = function(isOk) {
-		function acction(event) {
-			if (event.target.localName == 'tab' && event.button == 0) {
+		if (isOk) {
+			try {
+				gBrowser.mTabContainer.addEventListener('dblclick', TabPlus_mod.ldblclickR.acction, true);
+			} catch (e) {}
+		} else {
+			try {
+				gBrowser.mTabContainer.removeEventListener('dblclick', TabPlus_mod.ldblclickR.acction, true);
+			} catch (e) {}
+		}
+	};
+	TabPlus_mod.ldblclickR.acction = function(event) {
+		if (event.target.localName == 'tab' && event.button == 0) {
+			getBrowser().getBrowserForTab(event.target).reload();
+		}
+	};
+
+	TabPlus_mod.ldblclickRU = function(isOk) {
+		if (isOk) {
+			try {
+				gBrowser.mTabContainer.addEventListener('dblclick', acction, false);
+			} catch (e) {}
+		} else {
+			try {
+				gBrowser.mTabContainer.removeEventListener('dblclick', acction, false);
+			} catch (e) {}
+		}
+	};
+	TabPlus_mod.ldblclickRU.acction = function(event) {
+		if (event.target.localName == 'tab' && event.button == 0) {
+			if (event.target.hasAttribute("busy")) {
+				document.getElementById('cmd_close').doCommand();
+			} else {
 				getBrowser().getBrowserForTab(event.target).reload();
 			}
 		}
-		if (isOk) {
-			gBrowser.mTabContainer.addEventListener('dblclick', acction, true);
-		} else {
-			gBrowser.mTabContainer.removeEventListener('dblclick', acction, true);
-		}
 	};
-	TabPlus_mod.ldblclickRU = function(isOk) {
-		function acction(event) {
-			if (event.target.localName == 'tab' && event.button == 0) {
-				if (event.target.hasAttribute("busy")) {
-					document.getElementById('cmd_close').doCommand();
-				} else {
-					getBrowser().getBrowserForTab(event.target).reload();
-				}
-			}
-		}
-		if (isOk) {
-			gBrowser.mTabContainer.addEventListener('dblclick', acction, false);
-		} else {
-			gBrowser.mTabContainer.removeEventListener('dblclick', acction, false);
-		}
-	};
+
 	TabPlus_mod.nearTab = function(isOk) {
-		try {
-			if (!gBrowser) return;
-		} catch (e) {
-			return;
-		}
-
-		function tabOpenHandler(event) {
-			var tab = event.target;
-			gBrowser.moveTabTo(tab, gBrowser.mCurrentTab._tPos + 1);
-		}
 		if (isOk) {
-			gBrowser.tabContainer.addEventListener("TabOpen", tabOpenHandler, false);
+			try {
+				gBrowser.tabContainer.addEventListener("TabOpen", TabPlus_mod.nearTab.acction, false);
+			} catch (e) {}
 		} else {
-			gBrowser.tabContainer.removeEventListener("TabOpen", tabOpenHandler, false);
+			try {
+				gBrowser.tabContainer.removeEventListener("TabOpen", TabPlus_mod.nearTab.acction, false);
+			} catch (e) {}
 		}
 	};
-	TabPlus_mod.cOLTab = function(isOk) {
+	TabPlus_mod.nearTab.acction = function(event) {
 		try {
 			if (!gBrowser) return;
 		} catch (e) {
 			return;
 		}
+		var tab = event.target;
+		gBrowser.moveTabTo(tab, gBrowser.mCurrentTab._tPos + 1);
+	};
 
-		function tabCloseHandler(event) {
-			var tab = event.target;
-			if (tab.linkedBrowser.contentDocument.URL == 'about:blank') return;
-			if (tab._tPos <= gBrowser.mTabContainer.selectedIndex) {
-				if (tab.previousSibling) {
-					gBrowser.mTabContainer.selectedIndex--;
-				}
+	TabPlus_mod.cOLTab = function(isOk) {
+		if (isOk) {
+			try {
+				gBrowser.tabContainer.addEventListener("TabClose", TabPlus_mod.cOLTab.acction, false);
+			} catch (e) {}
+		} else {
+			try {
+				gBrowser.tabContainer.removeEventListener("TabClose", TabPlus_mod.cOLTab.acction, false);
+			} catch (e) {}
+		}
+	};
+	TabPlus_mod.cOLTab.acction = function(event) {
+		try {
+			if (!gBrowser) return;
+		} catch (e) {
+			return;
+		}
+		var tab = event.target;
+		if (tab.linkedBrowser.contentDocument.URL == 'about:blank') return;
+		if (tab._tPos <= gBrowser.mTabContainer.selectedIndex) {
+			if (tab.previousSibling) {
+				gBrowser.mTabContainer.selectedIndex--;
 			}
 		}
+	};
+
+	//--------------------------------------------------------------------------
+
+	TabPlus_mod.KeepBookmarksOnMiddleClick = function(isOk) {
 		if (isOk) {
-			gBrowser.tabContainer.addEventListener("TabClose", tabCloseHandler, false);
+			try {
+				eval('BookmarksEventHandler.onClick =' + this.cKBOnMCa.replace('node.hidePopup()', ''));
+				eval('checkForMiddleClick =' + this.cKBOnMCb.replace('closeMenus(event.target);', ''));
+			} catch (e) {}
 		} else {
-			gBrowser.tabContainer.removeEventListener("TabClose", tabCloseHandler, false);
+			try {
+				eval('BookmarksEventHandler.onClick =' + this.cKBOnMCa);
+				eval('checkForMiddleClick =' + this.cKBOnMCb);
+			} catch (e) {}
 		}
 	};
+
+	TabPlus_mod.closeDownloadBankTab = function(isOk) {
+		if (isOk) {
+			try {
+				eval("gBrowser.mTabProgressListener = " + this.cdbt.replace(/(?=var location)/, '\
+                if (aWebProgress.DOMWindow.document.documentURI == "about:blank"\
+                && aRequest.QueryInterface(nsIChannel).URI.spec != "about:blank") {\
+                aWebProgress.DOMWindow.setTimeout(function() {\
+                !aWebProgress.isLoadingDocument && aWebProgress.DOMWindow.close();\
+                }, 100);\
+                }\
+                '));
+			} catch (e) {}
+		} else {
+			try {
+				location == eval("gBrowser.mTabProgressListener=" + this.cdbt);
+			} catch (e) {}
+		}
+	};
+
+	TabPlus_mod.tabfocus = function(isOk) {
+		if (isOk) {
+			try {
+				gBrowser.tabContainer.addEventListener("mouseout", TabPlus_mod.tabfocus.onMouseOut, false);
+				gBrowser.tabContainer.addEventListener("mouseover", TabPlus_mod.tabfocus.onMouseOver, false);
+			} catch (e) {}
+		} else {
+			try {
+				gBrowser.tabContainer.removeEventListener("mouseover", TabPlus_mod.tabfocus.onMouseOver, false);
+				gBrowser.tabContainer.removeEventListener("mouseout", TabPlus_mod.tabfocus.onMouseOut, false);
+			} catch (e) {}
+		}
+	};
+	TabPlus_mod.tabfocus.onMouseOver = function(event) {
+		this.tab_hover = setTimeout(function() {
+			gBrowser.selectedTab = event.target;
+		}, 250);
+	};
+	TabPlus_mod.tabfocus.onMouseOut = function() {
+		clearTimeout(this.tab_hover);
+	};
+
 	TabPlus_mod.init();
 	window.TabPlus_mod = TabPlus_mod;
 })();
