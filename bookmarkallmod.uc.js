259bac90b51fe6486c907413fc2992e87274b412
FeiRuo <zhankongfeiruo@gmail.com>
2015-04-13 01:44:47 +0800

    优化代码，UI设置

--- bookmarkallmod.uc.js
+++ bookmarkallmod.uc.js
@@ -1,146 +1,326 @@
 // ==UserScript==
-// @name            bookmarkallmod
-// @description     浏览器退出时保存所有页面
-// @author          feiruo
-// @compatibility   Firefox 16
-// @charset         UTF-8
-// @include         main
-// @id              [77E1FF6F]
-// @startup         window.bookmarkallmod.init();
-// @shutdown        window.bookmarkallmod.onDestroy(true);
-// @reviewURL       http://bbs.kafan.cn/thread-1640643-1-1.html
-// @homepageURL     https://github.com/feiruo/userchromejs/
-// @note            注意：对于未载入标签，存入的书签名是URL，只有载入之后才能获取标题
-// @version         1.2.1
-// @version         1.2 修改动作方式，可以通过函数调用实时启用禁用和实时保存
-// @version         1.1 修复在没有书签保存的情况下仍然创建一个时间文件夹的问题
-// @version         1.0
+// @name			bookmarkallmod.uc.js
+// @description		浏览器退出时保存所有页面
+// @compatibility	Firefox16
+// @modby 			feiruo
+// @charset 		UTF-8
+// @include 		chrome://browser/content/browser.xul
+// @id 				[77E1FF6F]
+// @inspect 		window.BookmarkAll
+// @startup 		window.BookmarkAll.init();
+// @shutdown 		window.BookmarkAll.onDestroy();
+// @optionsURL		about:config?filter=BookmarkAll.
+// @config 			window.BookmarkAll.openPref();
+// @reviewURL		http://bbs.kafan.cn/thread-1640643-1-1.html
+// @homepageURL		https://github.com/feiruo/userChromeJS/
+// @downloadURL		https://github.com/feiruo/userChromeJS/blob/master/bookmarkallmod.uc.js
+// @version 		1.3 	2015.04.12 	10:00 	重建、优化代码。
+// @version 		1.2.1 	
+// @version 		1.2 	修改动作方式，可以通过函数调用实时启用禁用和实时保存
+// @version 		1.1 	修复在没有书签保存的情况下仍然创建一个时间文件夹的问题
+// @version 		1.0 
+// @note 			保存所有页面为书签
+// @note			注意：对于未载入标签，存入的书签名是URL，只有载入之后才能获取标题
 // ==/UserScript==
-
 /**
  * 参考bookmark_all_onclose(https://j.mozest.com/zh-CN/ucscript/script/58)
- **/
-(function() {
+ */
+location == "chrome://browser/content/browser.xul" && (function() {
+	if (window.BookmarkAll) {
+		window.BookmarkAll.onDestroy();
+		delete window.BookmarkAll;
+	}
+	var BookmarkAll = {
+		get BMSV() {
+			return Cc["@mozilla.org/browser/nav-bookmarks-service;1"].getService(Ci.nsINavBookmarksService);
+		},
+		get Time() {
+			var now = new Date();
+			return now.toLocaleFormat(this.DateParse);
+		},
 
-  if (window.bookmarkallmod) {
-    window.bookmarkallmod.onDestroy();
-    delete window.bookmarkallmod;
-  }
+		get prefs() {
+			delete this.prefs;
+			return this.prefs = Services.prefs.getBranch("userChromeJS.BookmarkAll.");
+		},
 
-  var bookmarkallmod = {
-    bookmarkallon: true, // 默认是否启用关闭窗口后自动保存，设为false之后可用快捷键调用
-    toolbarFolder: false, // 保存在书签工具栏为TRUE  否则保存在书签菜单里
-    number: "15", //条目限制，保存次数的上限，多余限制数目会自动删除旧的
-    dateParse: "%Y/%m/%d %H:%M:%S", // 日期格式,可以有空格之类的，年：%Y，月：%m，日：%d,，时：%H，分：%M，秒：%S
-    //文件目录，可以中文，有节操无下限
-    dirs: [
-      "Conversations",
+		get Window() {
+			var windowsMediator = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+				.getService(Components.interfaces.nsIWindowMediator);
+			return windowsMediator.getMostRecentWindow("BookmarkAll:Preferences");
+		},
 
-    ],
-    //不保存的网站，支持部分正则，
-    exclude: [
-      "chrome://*",
-      "resource://*",
-      "about:*",
-      "http://www.baidu.com/baidu?*",
-      "http://www.baidu.com/s?*",
-    ],
-  };
+		init: function() {
+			var ins = $("devToolsSeparator");
+			ins.parentNode.insertBefore($C("menuitem", {
+				id: "BookmarkAll_set",
+				label: "BookmarkAll配置",
+				oncommand: "BookmarkAll.openPref();",
+				class: "menuitem-iconic",
+			}), ins);
 
-  bookmarkallmod.init = function() {
-    if (bookmarkallmod.bookmarkallon)
-      window.addEventListener("unload", bookmarkallmod.bookmarkall, false);
-  };
+			this.loadSetting();
+			this.prefs.addObserver('', this.PrefsObs, false);
+			window.addEventListener("unload", BookmarkAll.Bookmark, false);
+		},
 
-  bookmarkallmod.onDestroy = function() {
-    window.removeEventListener("unload", bookmarkallmod.bookmarkall, false);
-  };
+		onDestroy: function() {
+			window.removeEventListener("unload", BookmarkAll.Bookmark, false);
+		},
 
-  bookmarkallmod.bookmarkall = function() {
-    var bookmarksService = Cc["@mozilla.org/browser/nav-bookmarks-service;1"].getService(Ci.nsINavBookmarksService);
-    if (bookmarkallmod.toolbarFolder == true) {
-      var target_folder = bookmarksService.toolbarFolder;
-    } else {
-      var target_folder = bookmarksService.bookmarksMenuFolder;
-    }
-    for (var i = 0; i < bookmarkallmod.dirs.length; i++) {
-      for (var j = 0;
-        (bookmarksService.getIdForItemAt(target_folder, j) != -1); j++) {
-        var temp_folder = bookmarksService.getIdForItemAt(target_folder, j);
-        var temp_folder_title = bookmarksService.getItemTitle(temp_folder);
-        if (temp_folder_title == bookmarkallmod.dirs[i]) {
-          target_folder = temp_folder;
-          var k = i;
-        }
-      }
-    }
-    if (k == undefined) {
-      for (i = 0; i < bookmarkallmod.dirs.length; i++) {
-        target_folder = bookmarksService.createFolder(target_folder, bookmarkallmod.dirs[i], 0);
-      }
-    } else {
-      for (i = 1 + k; i < bookmarkallmod.dirs.length; i++) {
-        target_folder = bookmarksService.createFolder(target_folder, bookmarkallmod.dirs[i], 0);
-      }
-    }
-    var duoyu = new Array();
-    for (var i = 0;
-      (bookmarksService.getIdForItemAt(target_folder, i) != -1); i++) {
-      var temp_folder = bookmarksService.getIdForItemAt(target_folder, i);
-      var temp_folder_title = bookmarksService.getItemTitle(temp_folder);
-      duoyu += temp_folder + "-";
-    }
-    if (i > bookmarkallmod.number - 2) {
-      var duoy = duoyu.substring(0, duoyu.lastIndexOf('-'));
-      var duo = duoy.split("-");
-      for (var m = duo.length - 1; m > bookmarkallmod.number - 2; m--) {
-        bookmarksService.removeItem(duo[m]);
-      }
-    }
+		PrefsObs: function(subject, topic, data) {
+			if (topic == 'nsPref:changed') {
+				switch (data) {
+					case 'Enable':
+					case 'ToolbarFolder':
+					case 'Index':
+					case 'DateParse':
+					case 'Dirs':
+					case 'Exclude':
+						BookmarkAll.loadSetting(data);
+						break;
+				}
+			}
+		},
 
-    function getDateTime() {
-      var now = new Date();
-      return now.toLocaleFormat(bookmarkallmod.dateParse);
-    }
+		loadSetting: function(type) {
+			if (!type || type === "Enable")
+				this.Enable = this.getPrefs(0, "Enable", false);
 
-    var www = new　 Array();
-    var browsers = document.getElementById('content').browsers;
-    for (var i = 0; i < browsers.length; ++i) {
-      var webNav = browsers[i].webNavigation;
-      var url = webNav.currentURI.spec;
-      var name = "";
-      try {
-        var doc = webNav.document;
-        name = doc.title || url;
-      } catch (e) {
-        name = url;
-      }
-      www += url + '::::::::::' + name + ',\n';
-    }
-    for (var i = 0; i < bookmarkallmod.exclude.length; i++) {
-      if (bookmarkallmod.exclude[i].match(/\*/)) {
-        var paichu = bookmarkallmod.exclude[i].replace(/\*/, ".*\n");
-        paichu = new RegExp(paichu, "g");
-      } else {
-        var paichu = bookmarkallmod.exclude[i] + ".*\n";
-        paichu = new RegExp(paichu, "");
-      }
-      if (www.match(paichu)) {
-        www = www.replace(paichu, "");
-      }
-    }
-    if (www.length != 0) {
-      var duoy = www.substring(0, www.lastIndexOf(',\n')).split(',\n');
-      target_folder = bookmarksService.createFolder(target_folder, getDateTime(), 0);
-      for (i = 0; i < duoy.length; i++) {
-        var wwww = duoy[i].substring(0, duoy[i].lastIndexOf('::::::::::'));
-        var tttt = duoy[i].substring(10 + duoy[i].indexOf('::::::::::'), duoy[i].length);
-        var uri = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService).newURI(wwww, null, null);
-        bookmarksService.insertBookmark(target_folder, uri, -1, tttt);
-      }
-    }
-  };
+			if (!type || type === "ToolbarFolder")
+				this.ToolbarFolder = this.getPrefs(0, "ToolbarFolder", false);
 
-  bookmarkallmod.init();
-  window.bookmarkallmod = bookmarkallmod;
+			if (!type || type === "Index")
+				this.Index = this.getPrefs(1, "Index", 15);
+
+			if (!type || type === "DateParse")
+				this.DateParse = this.getPrefs(2, "DateParse", "%Y/%m/%d %H:%M:%S");
+
+			if (!type || type === "Dirs") {
+				var Dirs = this.getPrefs(2, "Dirs", "Conversations");
+				Dirs = Dirs.replace(/\//g, '\\').split('\\');;
+				this.Dirs = Dirs;
+			}
+
+			if (!type || type === "Exclude") {
+				var Exclude = this.getPrefs(2, "Exclude", "chrome://, resource://, about:, http://www.baidu.com/baidu?, http://www.baidu.com/s?");
+				Exclude = Exclude.trim();
+				if (Exclude.indexOf(',') != -1)
+					Exclude = Exclude.replace(/,\s*/g, '\n');
+				this.prefs.setCharPref("Exclude", Exclude);
+				Exclude = Exclude.split(/\n+/).map(function(s) s.trim());
+				this.Exclude = Exclude;
+			}
+		},
+
+		/*****************************************************************************************/
+		Bookmark: function(isAlert) {
+			var that = BookmarkAll;
+
+			if (!that.Enable) return;
+
+			if (that.toolbarFolder)
+				TF = that.BMSV.toolbarFolder;
+			else
+				TF = that.BMSV.bookmarksMenuFolder;
+
+			var historyService = Cc["@mozilla.org/browser/nav-history-service;1"].getService(Ci.nsINavHistoryService),
+				options = historyService.getNewQueryOptions(),
+				query = historyService.getNewQuery();
+
+			query.setFolders([TF], 1);
+			var result = historyService.executeQuery(query, options),
+				rootNode = result.root;
+
+			rootNode.containerOpen = true;
+			var TTF = that.FindFolders(rootNode);
+			rootNode.containerOpen = false;
+
+			if (TTF)
+				TF = TTF.itemId;
+			else
+				TF = that.BMSV.createFolder(TF, that.Dirs[i], 0);
+
+
+			var TFT = that.BMSV.createFolder(TF, that.Time, 0);
+
+			var isTF = that.Exce(TFT);
+
+			if (!isTF)
+				return that.BMSV.removeItem(TFT);
+			that.DelOld(TF);
+		},
+
+		FindFolders: function(TF, dir) {
+			var TFs, nu = 0;
+
+			function FindFolder(TF, Dirs) {
+				for (var i = 0; i < TF.childCount; i++) {
+					var node = TF.getChild(i);
+					if (node.type != 6) continue;
+					if (node instanceof Ci.nsINavHistoryContainerResultNode) {
+						if (node.title != Dirs[nu]) continue;
+						nu = nu + 1;
+						TFs = node;
+						if (!Dirs[nu]) break;
+						var oldOpen = node.containerOpen;
+						node.containerOpen = true;
+						FindFolder(node, Dirs);
+						node.containerOpen = oldOpen;
+						break;
+					}
+				}
+			}
+
+			FindFolder(TF, this.Dirs);
+			return TFs;
+		},
+
+		Exce: function(TFT) {
+			var that = BookmarkAll;
+			var isTF;
+			var browsers = document.getElementById('content').browsers;
+			for (var i = 0; i < browsers.length; ++i) {
+				var webNav = browsers[i].webNavigation;
+				var url = webNav.currentURI.spec;
+				var title = "";
+				try {
+					var doc = webNav.document;
+					title = doc.title || url;
+				} catch (e) {
+					title = url;
+				}
+				for (var j in that.Exclude) {
+					var rex = new RegExp(that.Exclude[j]);
+					if (rex.test(url))
+						var isEx = true;
+				}
+				if (isEx) continue;
+				isTF = true;
+				that.BMSV.insertBookmark(TFT, webNav.currentURI, -1, title);
+			}
+			return isTF;
+		},
+
+		DelOld: function(TF) {
+			var Old = [];
+			for (var i = 0;
+				(this.BMSV.getIdForItemAt(TF, i) != -1); i++) {
+				var temp_folder = this.BMSV.getIdForItemAt(TF, i);
+				Old.push(temp_folder);
+			}
+			if (Old <= this.Index) return;
+			for (var j = this.Index; j < Old.length; j++) {
+				this.BMSV.removeItem(Old[j]);
+			}
+		},
+		/*****************************************************************************************/
+		getPrefs: function(type, name, val) {
+			if (type === 0) {
+				if (!this.prefs.prefHasUserValue(name) || this.prefs.getPrefType(name) != Ci.nsIPrefBranch.PREF_BOOL)
+					this.prefs.setBoolPref(name, val);
+				return this.prefs.getBoolPref(name);
+			}
+			if (type === 1) {
+				if (!this.prefs.prefHasUserValue(name) || this.prefs.getPrefType(name) != Ci.nsIPrefBranch.PREF_INT)
+					this.prefs.setIntPref(name, val);
+				return this.prefs.getIntPref(name);
+			}
+			if (type === 2) {
+				if (!this.prefs.prefHasUserValue(name) || this.prefs.getPrefType(name) != Ci.nsIPrefBranch.PREF_STRING)
+					this.prefs.setCharPref(name, val);
+				return this.prefs.getCharPref(name);
+			}
+		},
+
+		openPref: function() {
+			if (this.Window)
+				this.Window.focus();
+			else {
+				var option = this.option();
+				window.openDialog("data:application/vnd.mozilla.xul+xml;charset=UTF-8," + option, '', 'chrome,titlebar,toolbar,centerscreen,dialog=no');
+			}
+		},
+		option: function() {
+			xul = '<?xml version="1.0"?><?xml-stylesheet href="chrome://global/skin/" type="text/css"?>\
+				<prefwindow xmlns="http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul"\
+				id="BookmarkAll_Settings"\
+				ignorekeys="true"\
+				title="BookmarkAll 配置"\
+				buttons="accept,cancel,extra1"\
+				ondialogextra1="Resets();"\
+				windowtype="BookmarkAll:Preferences">\
+					<prefpane id="main" flex="1">\
+						<preferences>\
+							<preference id="Enable" type="bool" name="userChromeJS.BookmarkAll.Enable"/>\
+							<preference id="ToolbarFolder" type="bool" name="userChromeJS.BookmarkAll.ToolbarFolder"/>\
+							<preference id="Index" type="int" name="userChromeJS.BookmarkAll.Index"/>\
+							<preference id="DateParse" type="string" name="userChromeJS.BookmarkAll.DateParse"/>\
+							<preference id="Dirs" type="string" name="userChromeJS.BookmarkAll.Dirs"/>\
+							<preference id="Exclude" type="string" name="userChromeJS.BookmarkAll.Exclude"/>\
+						</preferences>\
+						<script>\
+							function Resets() {\
+								$("Enable").value = false;\
+								$("ToolbarFolder").value = false;\
+								$("Index").value = 15;\
+								$("DateParse").value = "%Y/%m/%d %H:%M:%S";\
+								$("Dirs").value = "Conversations";\
+								$("Exclude").value = "chrome://\\nresource://\\nabout:\\nhttp://www.baidu.com/baidu?\\nhttp://www.baidu.com/s?";\
+							}\
+							function $(id) document.getElementById(id);\
+						</script>\
+						<groupbox>\
+							<checkbox id="Enable" label="是否启用关闭窗口后自动保存" preference="Enable"/>\
+							<checkbox id="ToolbarFolder" label="保存在书签工具栏,否则保存在书签菜单" preference="ToolbarFolder"/>\
+						</groupbox>\
+						<groupbox>\
+							<grid>\
+								<rows>\
+									<row align="center">\
+										<label value="条目限制"/>\
+										<textbox id="Index" type="number" preference="Index"  tooltiptext="保存次数的上限，多余限制数目会自动删除旧的"/>\
+									</row>\
+									<row align="center">\
+										<label value="日期格式："/>\
+										<textbox id="DateParse" tooltiptext="可以有空格之类的，年：%Y，月：%m，日：%d,，时：%H，分：%M，秒：%S" preference="DateParse"/>\
+									</row>\
+									<row align="center">\
+										<label value="书签保存目录："/>\
+										<textbox id="Dirs" preference="Dirs" tooltiptext="多级请用 / 分割，如：目录1/子目录" style="width:400px;"/>\
+									</row>\
+								</rows>\
+							</grid>\
+						</groupbox>\
+						<groupbox>\
+							<caption label="排除列表，支持部分正则"/>\
+							<vbox flex="1" style="height:200px;">\
+								<textbox flex="1" preference="Exclude" tooltiptext="一行一个" multiline="true" cols="60"/>\
+							</vbox>\
+						</groupbox>\
+						<hbox flex="1">\
+							<button dlgtype="extra1" label="还原默认值" />\
+							<spacer flex="1" />\
+							<button dlgtype="accept"/>\
+							<button dlgtype="cancel"/>\
+						</hbox>\
+					</prefpane>\
+				</prefwindow>\
+				';
+			return encodeURIComponent(xul);
+		},
+	};
+	/*****************************************************************************************/
+	function $(id) {
+		return document.getElementById(id);
+	}
+
+	function $C(name, attr) {
+		var el = document.createElement(name);
+		if (attr) Object.keys(attr).forEach(function(n) el.setAttribute(n, attr[n]));
+		return el;
+	}
+
+	BookmarkAll.init();
+	window.BookmarkAll = BookmarkAll;
 })();
