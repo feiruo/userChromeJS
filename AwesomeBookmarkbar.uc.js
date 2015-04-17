f0fd0f7ec38ac4e20ba94500229a9a49bdda240f
FeiRuo <feiruo@users.noreply.github.com>
2015-04-17 12:11:05 +0800

    增加几个动作

--- AwesomeBookmarkbar.uc.js
+++ AwesomeBookmarkbar.uc.js
@@ -17,6 +17,7 @@
 // @note         	点击地址栏显示书签工具栏。
 // @note         	地址栏任意按键，地址栏失去焦点后自动隐藏书签工具栏。
 // @note       		左键点击书签后自动隐藏书签工具栏。
+// @version      	0.3.1	2015.04.17 10:00 	更多功能，表达能力略微提升.
 // @version      	0.3		2015.04.11 20:00 	绘制UI设置界面.
 // @version      	0.2.1 	去除鼠标移到地址栏自动显示书签工具栏
 // @version      	0.2 	增加鼠标移到地址栏自动显示书签工具栏，移出隐藏
@@ -44,6 +45,7 @@ location == "chrome://browser/content/browser.xul" && (function() {
 				id: "AwesomeBookmarkbar_set",
 				label: "智能书签栏配置",
 				oncommand: "AwesomeBookmarkbar.openPref();",
+				image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAByklEQVQ4jZ2Rz4sSYRjHn2yxYmlpqYV+LOye6uCtS3uu6LBFMJCJgviC0jonfxzUndMrHvI9hDiOh/eVGUE3kIEQHEedlx1eh7nVn1D/wF5rLwX9uCQsgRP6uTw8D3w/fOEBuIAQYkMIsXFhv+X7/rYQYt913XsQhGma14QQR67rItM0wwAAnuc9FkKczufzj57nPQsUOI6zyTn/wDnvTSaTKwAAs9ks6zjOD8757+l02hkOhzeWCjDGIdu292zb3vvbKDwej19alnUyGo18y7Ke9Hq9zWX5SwAA0Wj08mLqun4dAKDf798ZDAYHgfUNw3hoGMaxYRgZAABd1193u13R6XSCgwsopSnG2C9Kqd9ut28zxiTG2Dml1FdVdScwjDEOqar6VFXVsaZpx5qm3cQYh1qtVq7ZbL5tNBpvCCG7/21BCNnFGG/9c3tFCDmv1+vvMcbLP7CMSqWyXavVTqrV6k+M8TuE0NWVJaVS6a6iKDNFUb6Xy+XS4lMrkcvlHhSLxU+FQuFbPp9HKwsAAGRZPpBl+Us2mz3LZDKHa0nS6fRzhNBZKpX6nEwmH60lSSQSKB6Pf43FYmQtQSQSCUuS9EKSpPsAAH8A5AfFG6aqwRIAAAAASUVORK5CYII=",
 				class: "menuitem-iconic",
 			}), ins);
 
@@ -55,16 +57,20 @@ location == "chrome://browser/content/browser.xul" && (function() {
 		},
 
 		onDestroy: function() {
-			this.AddListener(false, 0, "command", "PersonalToolbarClick0", "Hide");
-			this.AddListener(false, 0, "click", "PersonalToolbarClick1", "Hide");
-			this.AddListener(false, 1, "command", "PersonalToolbarClick2", "Hide");
-			this.AddListener(false, 2, "click", "UrlbarClick", "CHide");
-			this.AddListener(false, 2, "mouseout", "UrlbarMouseout", "MHide");
-			this.AddListener(false, 2, "keydown", "UrlbarKey", "keyHide");
-			this.AddListener(false, 2, "blur", "UrlbarBlur", "MHide");
-			this.AddListener(false, 0, "mouseover", "PersonalToolbarMouseover", "MShow");
-			this.AddListener(false, 0, "mouseout", "PersonalToolbarMouseout", "MHide");
-			setToolbarVisibility($("PersonalToolbar"), true);
+			this.AddListener(false, 2, "click", "UClick_S", "Show");
+			this.AddListener(false, 2, "click", "UClick_H", "Hide");
+			this.AddListener(false, 2, "dblclick", "UDblclick_S", "Show");
+			this.AddListener(false, 2, "dblclick", "UDblclick_H", "Hide");
+			this.AddListener(false, 2, "blur", "UBlur_H", "Hide");
+			this.AddListener(false, 2, "keydown", "UKey_H", "keyHide");
+			this.AddListener(false, 2, "mouseover", "UMove_S", "MShow");
+			this.AddListener(false, 2, "mouseout", "UMouseout_H", "MHide");
+			this.AddListener(false, 0, "mouseover", "PMouseover_S", "MShow");
+			this.AddListener(false, 0, "mouseout", "PMouseout_H", "MHide");
+			this.AddListener(false, 0, "command", "PersonalToolbarClick0", "PHide");
+			this.AddListener(false, 0, "click", "PersonalToolbarClick1", "PHide");
+			this.AddListener(false, 1, "command", "PersonalToolbarClick2", "PHide");
+			this.ShowToolbar(true);
 			this.prefs.removeObserver('', this.PrefsObs, false);
 			if (this.Window) this.Window.close();
 			Services.obs.notifyObservers(null, "startupcache-invalidate", "");
@@ -73,13 +79,20 @@ location == "chrome://browser/content/browser.xul" && (function() {
 		PrefsObs: function(subject, topic, data) {
 			if (topic == 'nsPref:changed') {
 				switch (data) {
-					case 'PersonalToolbarClick':
-					case 'PersonalToolbarMouseover':
-					case 'PersonalToolbarMouseout':
-					case 'UrlbarClick':
-					case 'UrlbarMouseout':
-					case 'UrlbarKey':
-					case 'UrlbarBlur':
+					case 'PClick_H':
+					case 'PMouseover_S':
+					case 'PMouseout_H':
+					case 'UClick_H':
+					case 'UMouseout_H':
+					case 'UKey_H':
+					case 'UBlur_H':
+					case 'UDblclick_S':
+					case 'UDblclick_H':
+					case 'UClick_S':
+					case 'UMove_S':
+					case 'LShow':
+					case 'Hide_Time':
+					case 'Show_Time':
 						AwesomeBookmarkbar.loadSetting(data);
 						break;
 				}
@@ -87,29 +100,52 @@ location == "chrome://browser/content/browser.xul" && (function() {
 		},
 
 		loadSetting: function(type) {
-			if (!type || type === "PersonalToolbarClick") {
-				this.AddListener(this.getPrefs("PersonalToolbarClick"), 0, "command", "PersonalToolbarClick0", "Hide");
-				this.AddListener(this.getPrefs("PersonalToolbarClick"), 0, "click", "PersonalToolbarClick1", "Hide");
-				this.AddListener(this.getPrefs("PersonalToolbarClick"), 1, "command", "PersonalToolbarClick2", "Hide");
+			if (!type || type === "LShow") {
+				this.LShow = this.getPrefs(0, "LShow");
+				if (type) return;
+				this.ShowToolbar(this.LShow);
 			}
 
-			if (!type || type === "UrlbarClick")
-				this.AddListener(this.getPrefs("UrlbarClick"), 2, "click", "UrlbarClick", "CHide");
+			if (!type || type === "Hide_Time")
+				this.Hide_Time = this.getPrefs(1, "Hide_Time");
 
-			if (!type || type === "UrlbarMouseout")
-				this.AddListener(this.getPrefs("UrlbarMouseout"), 2, "mouseout", "UrlbarMouseout", "MHide");
+			if (!type || type === "Show_Time")
+				this.Show_Time = this.getPrefs(1, "Show_Time");
 
-			if (!type || type === "UrlbarKey")
-				this.AddListener(this.getPrefs("UrlbarKey"), 2, "keydown", "UrlbarKey", "keyHide");
+			if (!type || type === "UClick_S")
+				this.AddListener(this.getPrefs(0, "UClick_S"), 2, "click", "UClick_S", "Show");
 
-			if (!type || type === "UrlbarBlur")
-				this.AddListener(this.getPrefs("UrlbarBlur"), 2, "blur", "UrlbarBlur", "MHide");
+			if (!type || type === "UClick_H")
+				this.AddListener(this.getPrefs(0, "UClick_H"), 2, "click", "UClick_H", "Hide");
 
-			if (!type || type === "PersonalToolbarMouseover")
-				this.AddListener(this.getPrefs("PersonalToolbarMouseover"), 0, "mouseover", "PersonalToolbarMouseover", "MShow");
+			if (!type || type === "UDblclick_S")
+				this.AddListener(this.getPrefs(0, "UDblclick_S"), 2, "dblclick", "UDblclick_S", "Show");
 
-			if (!type || type === "PersonalToolbarMouseout")
-				this.AddListener(this.getPrefs("PersonalToolbarMouseout"), 0, "mouseout", "PersonalToolbarMouseout", "MHide");
+			if (!type || type === "UDblclick_H")
+				this.AddListener(this.getPrefs(0, "UDblclick_H"), 2, "dblclick", "UDblclick_H", "Hide");
+
+			if (!type || type === "UBlur_H")
+				this.AddListener(this.getPrefs(0, "UBlur_H"), 2, "blur", "UBlur_H", "Hide");
+
+			if (!type || type === "UKey_H")
+				this.AddListener(this.getPrefs(0, "UKey_H"), 2, "keydown", "UKey_H", "keyHide");
+
+			if (!type || type === "UMove_S")
+				this.AddListener(this.getPrefs(0, "UMove_S"), 2, "mouseover", "UMove_S", "Show");
+
+			if (!type || type === "UMouseout_H") {
+				this.AddListener(this.getPrefs(0, "UMouseout_H"), 2, "mouseout", "UMouseout_H", "Hide");
+				this.AddListener(this.getPrefs(0, "UMouseout_H"), 0, "mouseover", "PMouseover_S", "Show");
+			}
+
+			if (!type || type === "PMouseout_H")
+				this.AddListener(this.getPrefs(0, "PMouseout_H"), 0, "mouseout", "PMouseout_H", "Hide");
+
+			if (!type || type === "PClick_H") {
+				this.AddListener(this.getPrefs(0, "PClick_H"), 0, "command", "PersonalToolbarClick0", "PHide");
+				this.AddListener(this.getPrefs(0, "PClick_H"), 0, "click", "PersonalToolbarClick1", "PHide");
+				this.AddListener(this.getPrefs(0, "PClick_H"), 1, "command", "PersonalToolbarClick2", "PHide");
+			}
 		},
 
 		/*****************************************************************************************/
@@ -139,41 +175,59 @@ location == "chrome://browser/content/browser.xul" && (function() {
 			if (Tid == 'notification-popup-box' || Tid == 'identity-box' || Tid == 'urlbar-display-box' || Tid == 'urlbar-icons')
 				paid = true;
 			switch (command) {
-				case "Hide":
-					if (e.button == 2 || e.button == 1 || (e.button == 0 && !(e.metaKey || e.shiftKey || e.ctrlKey))) return;
-					this.HideToolbar(true);
-					break;
-				case "CHide":
-					if (e.button !== 0 || paid) return;
-					this.HideToolbar(false);
-					break;
-				case "MShow":
-					this.HideToolbar(false);
-					break;
-				case "MHide":
+				case "Show":
 					if (paid) return;
-					this.HideToolbar(true);
+					this.ShowToolbar(true);
+					break;
+				case "Hide":
+					if (paid) return;
+					this.ShowToolbar(false);
+					break;
+				case "PHide":
+					if (e.button == 2 || e.button == 1 || (e.button == 0 && !(e.metaKey || e.shiftKey || e.ctrlKey))) return;
+					this.ShowToolbar(false);
 					break;
 				case "keyHide":
 					if (window.event ? e.keyCode : e.which)
-						this.HideToolbar(true);
+						this.ShowToolbar(false);
 					break;
 			}
 		},
 
-		HideToolbar: function(isHide) {
+		ShowToolbar: function(show) {
 			var Bar = $("PersonalToolbar");
-			if (isHide)
-				setToolbarVisibility(Bar, false);
-			else
-				setToolbarVisibility(Bar, true);
+			var time;
+			if (show) {
+				show = true;
+				time = this.Show_Time;
+			} else {
+				show = false;
+				time = this.Hide_Time;
+			}
+			setTimeout(function() {
+				setToolbarVisibility(Bar, show);
+			}, time);
 		},
 
 		/*****************************************************************************************/
-		getPrefs: function(name) {
-			if (!this.prefs.prefHasUserValue(name) || this.prefs.getPrefType(name) != Ci.nsIPrefBranch.PREF_BOOL)
-				this.prefs.setBoolPref(name, false);
-			return this.prefs.getBoolPref(name);
+		getPrefs: function(type, name, val) {
+			switch (type) {
+				case 0:
+					if (!this.prefs.prefHasUserValue(name) || this.prefs.getPrefType(name) != Ci.nsIPrefBranch.PREF_BOOL)
+						this.prefs.setBoolPref(name, false);
+					return this.prefs.getBoolPref(name);
+					break;
+				case 1:
+					if (!this.prefs.prefHasUserValue(name) || this.prefs.getPrefType(name) != Ci.nsIPrefBranch.PREF_INT)
+						this.prefs.setIntPref(name, 0);
+					return this.prefs.getIntPref(name);
+					break;
+				case 2:
+					if (!this.prefs.prefHasUserValue(name) || this.prefs.getPrefType(name) != Ci.nsIPrefBranch.PREF_STRING)
+						this.prefs.setCharPref(name, "");
+					return this.prefs.getCharPref(name);
+					break;
+			}
 		},
 
 		openPref: function() {
@@ -196,57 +250,68 @@ location == "chrome://browser/content/browser.xul" && (function() {
 					windowtype="AwesomeBookmarkbar:Preferences">\
 					<prefpane id="main" flex="1">\
 						<preferences>\
-							<preference id="PersonalToolbarClick" type="bool" name="userChromeJS.AwesomeBookmarkbar.PersonalToolbarClick"/>\
-							<preference id="PersonalToolbarMouseover" type="bool" name="userChromeJS.AwesomeBookmarkbar.PersonalToolbarMouseover"/>\
-							<preference id="PersonalToolbarMouseout" type="bool" name="userChromeJS.AwesomeBookmarkbar.PersonalToolbarMouseout"/>\
-							<preference id="UrlbarClick" type="bool" name="userChromeJS.AwesomeBookmarkbar.UrlbarClick"/>\
-							<preference id="UrlbarMouseout" type="bool" name="userChromeJS.AwesomeBookmarkbar.UrlbarMouseout"/>\
-							<preference id="UrlbarKey" type="bool" name="userChromeJS.AwesomeBookmarkbar.UrlbarKey"/>\
-							<preference id="UrlbarBlur" type="bool" name="userChromeJS.AwesomeBookmarkbar.UrlbarBlur"/>\
+							<preference id="PClick_H" type="bool" name="userChromeJS.AwesomeBookmarkbar.PClick_H"/>\
+							<preference id="PMouseout_H" type="bool" name="userChromeJS.AwesomeBookmarkbar.PMouseout_H"/>\
+							<preference id="UClick_H" type="bool" name="userChromeJS.AwesomeBookmarkbar.UClick_H"/>\
+							<preference id="UMouseout_H" type="bool" name="userChromeJS.AwesomeBookmarkbar.UMouseout_H"/>\
+							<preference id="UKey_H" type="bool" name="userChromeJS.AwesomeBookmarkbar.UKey_H"/>\
+							<preference id="UBlur_H" type="bool" name="userChromeJS.AwesomeBookmarkbar.UBlur_H"/>\
+							<preference id="UDblclick_S" type="bool" name="userChromeJS.AwesomeBookmarkbar.UDblclick_S"/>\
+							<preference id="UDblclick_H" type="bool" name="userChromeJS.AwesomeBookmarkbar.UDblclick_H"/>\
+							<preference id="UClick_S" type="bool" name="userChromeJS.AwesomeBookmarkbar.UClick_S"/>\
+							<preference id="UMove_S" type="bool" name="userChromeJS.AwesomeBookmarkbar.UMove_S"/>\
+							<preference id="LShow" type="bool" name="userChromeJS.AwesomeBookmarkbar.LShow"/>\
+							<preference id="Hide_Time" type="int" name="userChromeJS.AwesomeBookmarkbar.Hide_Time"/>\
+							<preference id="Show_Time" type="int" name="userChromeJS.AwesomeBookmarkbar.Show_Time"/>\
 						</preferences>\
 						<script>\
 							function Resets() {\
-								$("PersonalToolbarClick").value = false;\
-								$("PersonalToolbarMouseover").value = false;\
-								$("PersonalToolbarMouseout").value = false;\
-								$("UrlbarClick").value = false;\
-								$("UrlbarMouseout").value = false;\
-								$("UrlbarKey").value = false;\
-								$("UrlbarBlur").value = false;\
-								opener.AwesomeBookmarkbar.HideToolbar();\
+								$("PClick_H").value = false;\
+								$("PMouseout_H").value = false;\
+								$("UClick_H").value = false;\
+								$("UMouseout_H").value = false;\
+								$("UKey_H").value = false;\
+								$("UBlur_H").value = false;\
+								$("UDblclick_S").value = false;\
+								$("UClick_S").value = false;\
+								$("UDblclick_H").value = false;\
+								$("UMove_S").value = false;\
+								$("LShow").value = false;\
+								$("Hide_Time").value = 0;\
+								$("Show_Time").value = 0;\
+								opener.AwesomeBookmarkbar.ShowToolbar();\
 							}\
 							function $(id) document.getElementById(id);\
 						</script>\
 						<groupbox>\
-							<caption label="显示书签工具栏"/>\
-							<checkbox id="PersonalToolbarMouseover" label="鼠标移入书签工具栏" preference="PersonalToolbarMouseover"/>\
-							<label value="或者直接显在标题栏右键显示书签工具栏"/>\
+							<checkbox id="LShow" label="启动时显示书签栏" preference="LShow"/>\
 						</groupbox>\
-						<groupbox>\
-							<caption label="隐藏书签工具栏"/>\
-							<grid>\
-								<rows>\
+						<hbox>\
+							<groupbox>\
+								<caption label="隐藏书签工具栏"/>\
 									<row align="center">\
-										<checkbox id="UrlbarMouseout" label="鼠标移出地址栏" preference="UrlbarMouseout"/>\
+										<label value="隐藏延时："/>\
+										<textbox id="Hide_Time" type="number" preference="Hide_Time" style="width:125px" tooltiptext="单位：毫秒！"/>\
 									</row>\
+									<checkbox id="UClick_H" label="地址栏点击" preference="UClick_H"/>\
+									<checkbox id="UDblclick_H" label="地址栏双击" preference="UDblclick_H"/>\
+									<checkbox id="UKey_H" label="地址栏任意按键" preference="UKey_H"/>\
+									<checkbox id="UBlur_H" label="地址栏失去焦点(页面内点击)" preference="UBlur_H"/>\
+									<checkbox id="UMouseout_H" label="鼠标移出地址栏" preference="UMouseout_H"/>\
+									<checkbox id="PMouseout_H" label="鼠标移出书签工具栏" preference="PMouseout_H"/>\
+									<checkbox id="PClick_H" label="书签工具栏点击书签之后" preference="PClick_H"/>\
+							</groupbox>\
+							<groupbox>\
+								<caption label="显示书签工具栏"/>\
 									<row align="center">\
-										<checkbox id="UrlbarClick" label="地址栏任意点击(鼠标)" preference="UrlbarClick"/>\
+										<label value="显示延时："/>\
+										<textbox id="Show_Time" type="number" preference="Show_Time" style="width:125px" tooltiptext="单位：毫秒！"/>\
 									</row>\
-									<row align="center">\
-										<checkbox id="UrlbarKey" label="地址栏任意按键(键盘)" preference="UrlbarKey"/>\
-									</row>\
-									<row align="center">\
-										<checkbox id="UrlbarBlur" label="地址栏失去焦点(页面内点击)" preference="UrlbarBlur"/>\
-									</row>\
-									<row align="center">\
-										<checkbox id="PersonalToolbarMouseout" label="鼠标移出书签工具栏" preference="PersonalToolbarMouseout"/>\
-									</row>\
-									<row align="center">\
-										<checkbox id="PersonalToolbarClick" label="书签工具栏点击书签之后" preference="PersonalToolbarClick"/>\
-									</row>\
-								</rows>\
-							</grid>\
-						</groupbox>\
+									<checkbox id="UDblclick_S" label="地址栏双击" preference="UDblclick_S"/>\
+									<checkbox id="UClick_S" label="地址栏单击" preference="UClick_S"/>\
+									<checkbox id="UMove_S" label="鼠标移入地址栏" preference="UMove_S"/>\
+							</groupbox>\
+						</hbox>\
 						<hbox flex="1">\
 							<button dlgtype="extra1" label="还原默认值" />\
 							<spacer flex="1" />\
